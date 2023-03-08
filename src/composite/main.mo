eb0e5a1b406954ef9d9f70003d1fd5c78ea63cb1
Hoimanti Dutta <hoimantidutta.cse2020@nsec.ac.in>
2023-03-09 00:32:03 +0530

    initial commit

---
+++ src/composite/main.mo
@@ -0,0 +1,54 @@
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+
+actor Composite {
+  stable var currentValue: Float = 300;
+  currentValue := 300;
+
+  // Debug.print("Hello, World!");
+
+  // let id = 123456;
+  // Debug.print(debug_show(id));
+
+  public func topUp(amount: Float){
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
+  };
+
+  public func withdraw(amount: Float){
+    let tempValue: Float = currentValue - amount;
+    if(tempValue >= 0){
+      currentValue -= amount;
+      Debug.print(debug_show(currentValue));
+    }
+    else{
+      Debug.print("Amount too large, current value less than zero!");
+    }
+  };
+
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  stable var startTime = Time.now();
+  Debug.print(debug_show(startTime));
+
+  public func compound(){
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
+
+  }
+
+  
+
+
+
+
+
+
+}
---
+++ src/composite_assets/src/index.js
@@ -0,0 +1,42 @@
+
+import { composite, dbank } from "../../declarations/composite";
+
+window.addEventListener("load", async function() {
+  // console.log("Finished loading");
+  update();
+});
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  // console.log("Submitted.");
+
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true);
+
+  if (document.getElementById("input-amount").value.length != 0) {
+    await composite.topUp(inputAmount);
+  }
+
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await composite.withdraw(outputAmount);
+  }
+
+  await composite.compound();
+
+  update();
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled");
+
+});
+
+async function update() {
+  const currentAmount = await composite.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
